a1a2435431dc48febd648183bd724fa9363fbea5
Enmanuel Aponte <enmanuelaponte1@gmail.com>
2019-05-06 21:55:05 +0000

    Book Final #2

--- book.js
+++ book.js
@@ -1,1 +1,30 @@
 // JavaScript File
+$(".search").click(function(){
+    var userinput = $("#titles").val();
+    var request_url = "https://www.googleapis.com/books/v1/volumes?q=" + userinput ;
+    $(".info").show();
+
+  $.ajax({
+      url: request_url ,
+      method: "GET",
+        success: function(response) {
+infobook = response.items[0].volumeInfo.description;
+infobook1 = response.items[0].volumeInfo.title;
+infobook2 = response.items[0].volumeInfo.publisher;
+infobook3 = response.items[0].volumeInfo.categories;
+infobook4 = response.items[0].volumeInfo.publisher;
+infobook5 = response.items[0].volumeInfo.pageCount;
+
+            $("#description").append(infobook);
+            $("#titleofbook").append(infobook1);
+            $("#author").append(infobook2);
+            $("#category").append(infobook3);
+            $("#publisher").append(infobook4);
+            $("#pagecount").append(infobook5);
+
+        }  
+
+   
+  });
+});
+$(".info").hide();
