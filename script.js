3ae77cf0790d3e9eb9e9cdb2f526b5a16c739ed5
Enmanuel Aponte <enmanuelaponte1@gmail.com>
2019-05-13 21:37:17 +0000

    Added Buy Button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // JavaScript File
-
 $(".search").click(function(){
     var userinput = $("#titles").val();
     var request_url = "https://www.googleapis.com/books/v1/volumes?q=" + userinput ;
@@ -15,7 +14,9 @@ infobook2 = response.items[0].volumeInfo.publisher;
 infobook3 = response.items[0].volumeInfo.categories;
 infobook4 = response.items[0].volumeInfo.publisher;
 infobook5 = response.items[0].volumeInfo.pageCount;
-infobook6 = response.items[0].volumeInfo.imageLinks[0];
+infobook6 = response.items[0].volumeInfo.imageLinks.thumbnail;
+infobook7 =response.items[2].saleInfo.buyLink
+
 
             $("#description").append(infobook);
             $("#titleofbook").append(infobook1);
@@ -23,7 +24,10 @@ infobook6 = response.items[0].volumeInfo.imageLinks[0];
             $("#category").append(infobook3);
             $("#publisher").append(infobook4);
             $("#pagecount").append(infobook5);
-            $("#picture").append("<img src=' + infobook6 + '>");
+            $("#picture").append("<img src=" + infobook6 +">");
+            debugger;
+            $("#buy").attr("href" , infobook7);
+
         }  
 
    
